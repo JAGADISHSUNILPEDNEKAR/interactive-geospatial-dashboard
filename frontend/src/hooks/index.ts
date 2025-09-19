345786605f784eb389a9f008bc77a16acb458962
JagadishSunilPednekar <jsphere16@gmail.com>
2025-09-19 10:58:42 +0530

    Added a frontend hook

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,8 @@
+// src/hooks/index.ts
+export { useVisualizationSync } from './useVisualizationSync';
+export { useWebSocket } from './useWebSocket';
+export { usePerformanceMonitoring } from './usePerformanceMonitoring';
+
+
+
+
